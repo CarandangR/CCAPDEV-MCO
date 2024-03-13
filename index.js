4d643ac622b3d538c672f853ec66c060c9c518f8
Peter <136557371+Wala3h@users.noreply.github.com>
2024-03-13 16:53:04 +0800

    testing handlebars things

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const exphbs = require('express-handlebars');
+const port = 3000;
 const app = express();
+
 app.use(express.static("./"));
+
+app.engine("hbs", exphbs.engine({extname: 'hbs'}));
+app.set("view engine", "hbs");
+
+
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/main.html");
+    res.sendFile(__dirname + "/views/main.html");
 });
+
 app.listen(3000, function () {
-    console.log("Server is running on localhost3000");
+    console.log("Server is running on localhost 3000");
 });
