349c5058f71397e24793bd5a216b0077761ce4e6
Wala <ptan9809@gmail.com>
2024-03-15 12:27:25 +0800

    EDIT DESC WORK

--- public/js/profile.js
+++ public/js/profile.js
@@ -15,15 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
     window.scrollTo(0, 0);
 
     console.log("document loaded");
-   document.querySelector(".editbutton").addEventListener("click", function(e) {
+   document.querySelector(".editbutton a").addEventListener("click", function(e) {
     e.preventDefault();
 
     let profiledescription = document.querySelector(".profiledesc");
     var textBox = document.createElement("input");
     //textBox.setAttribute("type", "text");
     textBox.value = profiledescription.innerText;
-    //textBox.setAttribute("value", profiledescription.innerText.trim());
-    profiledescription.parentNode.replaceChild(textBox, profiledescription);
+    profiledescription.appendChild(textBox);
     textBox.focus();
     let submitbutton = document.querySelector(".editbutton button");
     submitbutton.style.display = "block";
@@ -32,10 +31,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
         console.log("submitted!");
         submitbutton.style.display = "none"; // Hide the submit button after editing
-        profiledescription.innerText = textBox.value;
+        document.querySelector(".profiledesc p").innerText = textBox.value;
         
             // Revert back to the original content
-        textBox.replaceWith(profiledescription);
+        //textBox.replaceWith(profiledescription);
+        textBox.remove();
         
     });
    });
