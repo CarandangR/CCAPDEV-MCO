8be64553d0b4d83feb147662f42b061191bbe2c1
Wala <ptan9809@gmail.com>
2024-04-05 22:45:53 +0800

    Implementing upbit/downbit on maincommunity page
    Testing if it will work

--- public/js/main_community.js
+++ public/js/main_community.js
@@ -59,3 +59,98 @@ button.addEventListener('click', async function() {
         console.error(err);
     }
 });
+
+
+document.querySelectorAll('.postfooter').forEach(function(footer) {
+    const postContainerElement = footer.closest('.postscontainer');
+    const postId = postContainerElement.getAttribute('data-postid');
+
+    footer.querySelector('.upbit').addEventListener('click', async function () {
+        console.log("Upbit Clicked");
+        if(footer.querySelector('.upbit img').getAttribute('src').includes('/static/img/up-arrow-regular-24.png')) {
+            console.log("icnrease votes by 1 cuz regular img");
+            footer.querySelector('.upbit img').setAttribute('src', '/static/img/up-arrow-solid-24.png');
+            try {
+                    const response = await fetch(`/updatepostvote/` + postId, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ 
+                            type: 'upvote',
+                            id: postId,
+                         })
+                    });
+                    if (!response.ok) {
+                        console.error('Error updating vote:', response.statusText);
+                    }
+                } catch (error) {
+                    console.error('Error updating vote:', error);
+                }
+        }
+        else if(footer.querySelector('.upbit img').getAttribute('src').includes('/static/img/up-arrow-solid-24.png')) {
+            console.log("decrease votes by 1 cuz solid img");
+            footer.querySelector('.upbit img').setAttribute('src', '/static/img/up-arrow-regular-24.png');
+            try {
+                const response = await fetch(`/updatepostvote/` + postId, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ 
+                        type: 'upvote reduce',
+                        id: postId,
+                     })
+                });
+                if (!response.ok) {
+                    console.error('Error updating vote:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error updating vote:', error);
+            }
+        }
+    }); 
+    footer.querySelector('.downbit').addEventListener('click', async function() { 
+         console.log("Downbit Clicked");
+         if(footer.querySelector('.downbit img').getAttribute('src').includes('/static/img/down-arrow-regular-24.png')) {
+            footer.querySelector('.downbit img').setAttribute('src', '/static/img/down-arrow-solid-24.png');
+            try {
+                const response = await fetch(`/updatepostvote/` + postId, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ 
+                        type: 'downvote',
+                        id: postId,
+                     })
+                });
+                if (!response.ok) {
+                    console.error('Error updating vote:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error updating vote:', error);
+            }
+        }
+        else if(footer.querySelector('.downbit img').getAttribute('src').includes('/static/img/down-arrow-solid-24.png')) {
+            footer.querySelector('.downbit img').setAttribute('src', '/static/img/down-arrow-regular-24.png');
+            try {
+                const response = await fetch(`/updatepostvote/` + postId, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ 
+                        type: 'downvote reduce',
+                        id: postId,
+                     })
+                });
+                if (!response.ok) {
+                    console.error('Error updating vote:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error updating vote:', error);
+            }
+        }
+    });
+});
